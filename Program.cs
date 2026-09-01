ac0441f2ac2c693d902a6cba806770d9f68528e4
pbivens26 <bivensphillip967@gmail.com>
2026-08-31 18:06:53 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Program
+    {
+        static string name = "Phil!";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
